4b3ff568bb520f1bb76e86c57d1b134da35e8508
Moses Lee <moses_lee@apple.com>
2015-11-01 23:01:57 -0800

    Initial Commit

---
+++ 3DTouchPad/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  3DTouchPad
+//
+//  Created by Moses Lee on 11/1/15.
+//  Copyright © 2015 Moses Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 3DTouchPad/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  3DTouchPad
+//
+//  Created by Moses Lee on 11/1/15.
+//  Copyright © 2015 Moses Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
